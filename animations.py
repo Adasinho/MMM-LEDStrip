3c3979e113ab55f8bd5743f5b02a6dd4e83f99e2
Adasinho <adirm10@yahoo.com>
2019-03-22 11:42:54 +0100

    [3/n] Fix leds working and add unit tests

--- animations.py
+++ animations.py
@@ -198,7 +198,7 @@ def fade_out_from_current_brightness(led_strip, manager, wait_ms=10):
 
 def fade_out_from_current_brightness_no_trigger(led_strip, wait_ms=10):
     for i in range(led_strip.getBrightness(), 0, -1):
-        led_strip.getBrightness(i)
+        led_strip.setBrightness(i)
         led_strip.show()
         time.sleep(wait_ms / 1000.0)
 
